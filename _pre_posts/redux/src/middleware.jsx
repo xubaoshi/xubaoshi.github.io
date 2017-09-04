4984b32bbc7ca1ef3fd2d2b28c05af4ade4c655e
xubaoshi <948869516@qq.com>
2017-09-04 20:00:11 +0800

    add

--- _pre_posts/redux/src/middleware.jsx
+++ _pre_posts/redux/src/middleware.jsx
@@ -1,30 +1,114 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { connect , Provider } from 'react-redux'
-import { createStore,combineReducers  } from 'redux'
+import { connect, Provider } from 'react-redux';
+import { createStore, combineReducers } from 'redux';
 
 
 // constants
+const GET_MESSAGE_LIST = 'GET_MESSAGE_LIST';
+const ADD_MESSAGE = 'ADD_MESSAGE';
+const DELETE_MESSAGE = 'DELETE_MESSAGE';
+
 
 // reduce
-const reducer = (state=[],action) => {
-    switch(action.type){
+let time = new Date().getTime();
+const messagesReducer = (state = [{ id: time, name: `name${time}` }], action) => {
+    switch (action.type) {
+        case GET_MESSAGE_LIST:
+            return state;
+        case ADD_MESSAGE:
+            return [{ id: action.addTime, name: `name${action.addTime}` }, ...state];
+        case DELETE_MESSAGE:
+            const [a, ...newList] = state;
+            return newList;
         default:
-        return state
+            return state
     }
 }
-const store = combineReducers({
-
+const reducer = combineReducers({
+    messages: messagesReducer
 });
+const store = createStore(reducer);
 
-// action
+// actions
+const getMessageListAction = () => {
+    return {
+        type: GET_MESSAGE_LIST
+    }
+}
+const addMessageAction = (addTime) => {
+    return {
+        type: ADD_MESSAGE,
+        addTime
+    }
+}
+const deleteMessageAction = () => {
+    return {
+        type: DELETE_MESSAGE
+    }
+}
 
 // component
-class App extends React.Component{
-    render(){
+// UI 组件
+class MessageList extends React.Component {
+    constructor(props) {
+        super(props);
+        this.addHandle = this.addHandle.bind(this);
+        this.deleteHandle = this.deleteHandle.bind(this);
+    }
+    render() {
+        const { messages } = this.props;
+        const str = messages.map(obj =>
+             <p key={obj.id}>
+                <strong>id:</strong><span style={{'marginRight':'50px'}}>{obj.id}</span>
+                <strong>name:</strong><span>{obj.name}</span>
+             </p>
+        );
         return (
             <div>
-                <h1>Logger</h1>
+                {str}
+                <button onClick={this.addHandle}>add</button>
+                <button onClick={this.deleteHandle}>delete</button>
+            </div>
+        )
+    }
+    addHandle() {
+        const { addMessage } = this.props;
+        addMessage();
+    }
+    deleteHandle() {
+        const { deleteMessage } = this.props;
+        deleteMessage();
+    }
+}
+
+const mapStateToProps = (state, ownProps) => {
+    return {
+        messages: state.messages
+    }
+}
+
+const mapDispacthToProps = dispatch => {
+    return {
+        getMessages() {
+            store.dispatch(getMessageListAction());
+        },
+        addMessage() {
+            store.dispatch(addMessageAction(new Date().getTime()));
+        },
+        deleteMessage() {
+            store.dispatch(deleteMessageAction());
+        }
+    }
+}
+const MessageListWrapper = connect(mapStateToProps, mapDispacthToProps)(MessageList);
+
+// App
+class App extends React.Component {
+    render() {
+        return (
+            <div>
+                <MessageListWrapper></MessageListWrapper>
             </div>
         )
     }
--- _pre_posts/redux/webpack.config.js
+++ _pre_posts/redux/webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
         'react-hot-loader/patch',
         'webpack-dev-server/client?http://localhost:8080',
         'webpack/hot/only-dev-server',
-        './shouldComponentUpdate.jsx'
+        './middleware.jsx'
     ],
     output: {
         filename: 'bundle.js',
