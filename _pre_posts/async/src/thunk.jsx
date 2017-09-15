0dd3f56c07014ad126dd59b69fe2e19ec3f6ea3f
xubaoshi <948869516@qq.com>
2017-09-15 17:35:41 +0800

    add code

--- _pre_posts/async/src/thunk.jsx
+++ _pre_posts/async/src/thunk.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import { connect, Provider } from 'react-redux';
-import ReduxThunk from 'redux-thunk'
+import thunk from 'redux-thunk'
 import logger from 'redux-logger'
 import $ from 'jquery'
 
@@ -15,9 +15,9 @@ const FETCH_CNODE_SUCCESS = 'FETCH_CNODE_SUCCESS';
 const listReducer = (state = { isFetching: false, hasError: false, data: [] }, action) => {
     switch (action.type) {
         case FETCH_CNODE_LIST:
-            return Object.assign({}, state, { isFetching: true });
+            return Object.assign({}, state, { isFetching: true, hasError: false });
         case FETCH_CNODE_FAILURE:
-            return Object.assign({}, state, { hasError: true });
+            return Object.assign({}, state, { hasError: true, isFetching: false });
         case FETCH_CNODE_SUCCESS:
             return Object.assign({}, state, { data: action.data, isFetching: false, hasError: false });
         default:
@@ -25,10 +25,10 @@ const listReducer = (state = { isFetching: false, hasError: false, data: [] }, a
     }
 }
 const reducer = combineReducers({
-    messages: listReducer
+    cnode: listReducer
 });
 
-let createStoreWithMiddleware = applyMiddleware(ReduxThunk, logger);
+let createStoreWithMiddleware = applyMiddleware(thunk, logger)(createStore);
 let store = createStoreWithMiddleware(reducer);
 
 // actions
@@ -51,15 +51,19 @@ const fectchSuccess = (data) => {
 const fetchNodeList = ({ tab }) => {
     return dispatch => {
         dispatch(beginFectch());
-        $.get(`https://cnodejs.org/api/topics?tab=${tab}&limit=20`, function (result) {
-            if (result && result.data) {
-                if (Math.random() > 0.5) {
+        $.ajax({
+            url: `https://cnodejs.org/api/v1/topics?tab=${tab}&limit=20`,
+            success: function (result) {
+                if (result.success && result.data) {
                     dispatch(fectchSuccess(result.data));
                 } else {
                     dispatch(fectchFail());
                 }
+            },
+            error: function (jqXHR, textStatus, errorThrown) {
+                dispatch(fectchFail());
             }
-        })
+        });
     }
 }
 
@@ -68,30 +72,37 @@ const fetchNodeList = ({ tab }) => {
 class CnodeList extends React.Component {
     constructor(props) {
         super(props);
+        this.getNodeList = this.getNodeList.bind(this);
     }
     render() {
-        const { result } = this.props;
+        const { cnode } = this.props;
         var str = '';
-        if (result.isFetching) {
+        if (cnode.isFetching) {
             str = '正在查询。。。。';
-        } else if (result.hasError) {
+        }
+        else if (cnode.hasError) {
             str = '出现错误，请重试。。。';
         } else {
-            str = result.data.map(obj =>
-                <p key={obj.id}>
-                    <strong>id:</strong><span style={{ 'marginRight': '50px' }}>{obj.id}</span>
-                    <strong>author_id:</strong><span>{obj.author_id}</span>
-                </p>
-            );
-        }
+            if (cnode.data && cnode.data.length > 0) {
+                str = cnode.data.map(obj =>
+                    <li key={obj.id}>
+                        <strong>id:</strong><span style={{ 'marginRight': '50px' }}>{obj.id}</span>
+                        <strong>author_id:</strong><span>{obj.author_id}</span>
+                    </li>
+                );
+            } else {
+                str = <li>请查询。。。</li>;
+            }
 
+        }
         return (
             <div>
-                <div>{str}</div>
-                <button onClick={this.getNodeList.bind(this, 1)}>1</button>
-                <button onClick={this.getNodeList.bind(this, 2)}>2</button>
-                <button onClick={this.getNodeList.bind(this, 3)}>3</button>
-                <button onClick={this.getNodeList.bind(this, 4)}>4</button>
+                <button onClick={this.getNodeList.bind(this, 'ask')}>查询</button>
+                <ul>{str}</ul>
+                <button onClick={this.getNodeList.bind(this, 'ask')}>1</button>
+                <button onClick={this.getNodeList.bind(this, 'share')}>2</button>
+                <button onClick={this.getNodeList.bind(this, 'job')}>3</button>
+                <button onClick={this.getNodeList.bind(this, 'good')}>4</button>
             </div>
         )
     }
@@ -103,19 +114,19 @@ class CnodeList extends React.Component {
 
 const mapStateToProps = (state) => {
     return {
-        result: state
+        cnode: state.cnode
     }
 }
 
 const mapDispatchToProps = dispatch => {
     return {
         getNodeList(tab) {
-            dispatch(fetchNodeList(tab))
+            dispatch(fetchNodeList({ tab }))
         }
     }
 }
 
-const CnodeListWrapper = connect(mapStateToProps, mapStateToProps)(CnodeList);
+const CnodeListWrapper = connect(mapStateToProps, mapDispatchToProps)(CnodeList);
 
 class App extends React.Component {
     render() {
@@ -128,6 +139,8 @@ class App extends React.Component {
 }
 
 ReactDOM.render(
-    <App></App>,
+    <Provider store={store}>
+        <App></App>
+    </Provider>,
     document.getElementById('root')
 )
