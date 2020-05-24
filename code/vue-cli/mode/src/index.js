b1898bcf375bb1ba4bb895ba1cbb365b27fa03b8
xubaoshi <948869516@qq.com>
2020-05-24 13:16:53 +0800

    edit codfe

--- code/vue-cli/mode/src/index.js
+++ code/vue-cli/mode/src/index.js
@@ -4,3 +4,5 @@ import img from './img/big.png'
 var imgElement = document.createElement('img')
 imgElement.src = img
 document.body.appendChild(imgElement)
+
+console.log('process.env.NODE_ENV', process.env.NODE_ENV)
