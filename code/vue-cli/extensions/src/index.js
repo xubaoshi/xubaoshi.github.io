14b634892b145604843122bd5b19315eed444616
xubaoshi <948869516@qq.com>
2020-05-25 17:37:34 +0800

    add doc and demo code

---
+++ code/vue-cli/extensions/src/index.js
@@ -0,0 +1,9 @@
+import './css/style.css'
+import img from './img/big.png'
+import file from './test'
+
+var imgElement = document.createElement('img')
+imgElement.src = img
+document.body.appendChild(imgElement)
+
+console.log(file)
