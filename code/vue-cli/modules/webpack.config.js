601a16295ee871f65d6b76ab75e924ed41ae3e0c
xubaoshi <948869516@qq.com>
2020-05-26 09:33:17 +0800

    add vue-cli  doc

---
+++ code/vue-cli/modules/webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path')
+
+module.exports = {
+  entry: path.join(__dirname, 'src/index.js'),
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/dist/',
+  },
+  resolve: {
+    extensions: ['.ext', '.js', '.json'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|jpg)$/,
+        loader: 'url-loader',
+        options: {
+          limit: 3000,
+          name: '[name].[hash:8].[ext]',
+        },
+      },
+    ],
+  },
+}
