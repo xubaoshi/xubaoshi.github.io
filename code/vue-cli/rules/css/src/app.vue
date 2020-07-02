618070527f9d7a0157493ce6e17c87551d6e2804
xubaoshi <948869516@qq.com>
2020-07-02 20:43:43 +0800

    edit css demo code

---
+++ code/vue-cli/rules/css/src/app.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <div :class="$style.brown">
+      module
+    </div>
+    <div class="red">
+      scope
+    </div>
+    <div :class="moduleCss.blue">
+      module css
+    </div>
+  </div>
+</template>
+<script>
+import './css/style.css'
+import img from './img/big.png'
+import moduleCss from './css/style.module.css'
+export default {
+  name: 'App',
+  data() {
+    return {
+      moduleCss
+    }
+  },
+  mounted() {
+    var imgElement = document.createElement('img')
+    imgElement.src = img
+    imgElement.className = 'big'
+    document.body.appendChild(imgElement)
+    console.log(this.$style)
+  },
+}
+</script>
+<style module>
+.brown {
+  color: brown;
+}
+</style>
+<style scoped>
+.red {
+  color: red;
+}
+</style>
---
+++ code/vue-cli/rules/css/webpack.config.js
@@ -0,0 +1,119 @@
+const path = require('path')
+const VueLoaderPlugin = require('vue-loader/lib/plugin')
+const htmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  entry: path.join(__dirname, 'src/index.js'),
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/',
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '.vue', '.json'],
+  },
+  module: {
+    rules: [
+      // {
+      // css-loader 解析 css 模块、style-loader 将解析的 css-loader 插入到页面 head 中
+      //   test: /\.css$/,
+      //   use: ['style-loader', 'css-loader'],
+      // },
+      // {
+      //   // postcss-loader 可以使用 autoprefixer 针对不同浏览器添加前缀
+      //   // 需要配合 package.json 中的 browserslist 及 postcss.config.js
+      //   test: /\.css$/,
+      //   use: ['style-loader', 'css-loader', 'postcss-loader'],
+      // },
+      {
+        test: /\.css$/,
+        oneOf: [
+          {
+            resourceQuery: /module/,
+            use: [
+              { loader: 'vue-style-loader' },
+              {
+                loader: 'css-loader',
+                options: {
+                  importLoaders: 2,
+                  modules: {
+                    // 自定义生成的类名
+                    localIdentName: '[name]_[local]_[hash:base64:5]',
+                  },
+                },
+              },
+              { loader: 'postcss-loader' },
+            ],
+          },
+          {
+            resourceQuery: /\?vue/,
+            use: [
+              { loader: 'vue-style-loader' },
+              { loader: 'css-loader', options: { importLoaders: 2 } },
+              { loader: 'postcss-loader' },
+            ],
+          },
+          {
+            resourceQuery: /\.module\.\w+$/,
+            use: [
+              { loader: 'vue-style-loader' },
+              {
+                loader: 'css-loader',
+                options: {
+                  importLoaders: 2,
+                  modules: {
+                    // 自定义生成的类名
+                    localIdentName: '[name]_[local]_[hash:base64:5]',
+                  },
+                },
+              },
+            ],
+          },
+          {
+            use: ['style-loader', 'css-loader', 'postcss-loader'],
+          },
+        ],
+      },
+      {
+        test: /\.(png|jpg)$/,
+        loader: 'url-loader',
+        options: {
+          limit: 3000,
+          name: '[name].[hash:8].[ext]',
+        },
+      },
+      {
+        test: /\.vue$/,
+        use: [
+          {
+            loader: 'cache-loader',
+            options: {
+              cacheDirectory: path.resolve(
+                __dirname,
+                'node_modules/.cache/vue-loader'
+              ),
+              cacheIdentifier: 'test',
+            },
+          },
+          {
+            loader: 'vue-loader',
+            options: {
+              cacheDirectory: path.resolve(
+                __dirname,
+                'node_modules/.cache/vue-loader'
+              ),
+              cacheIdentifier: 'test',
+            },
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new VueLoaderPlugin(),
+    new htmlWebpackPlugin({
+      template: 'index.html',
+      filename: 'index.html',
+    }),
+  ],
+}
